1bcaa71d599847e063f41ad64710445f688cc44c
Hal Peters <geohalbert@gmail.com>
2020-03-21 21:42:29 -0500

    pods updated, new images, new readme

---
+++ ios/Pods/Target Support Files/Pods-QuakeCatalogMobile/Pods-QuakeCatalogMobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuakeCatalogMobile : NSObject
+@end
+@implementation PodsDummy_Pods_QuakeCatalogMobile
+@end
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,4 @@
 export default {
-	logo: require('./logo.png')
-}
-
-
-
-
+  logo: require("./logo.png"),
+  splash: require("./splash.png")
+};
