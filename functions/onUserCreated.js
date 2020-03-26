cae0c2101552793d3716145a7866a606496389ca
Hal Peters <geohalbert@gmail.com>
2020-03-25 23:27:17 -0500

    firestore setup, users update db

--- functions/onUserCreated.js
+++ functions/onUserCreated.js
@@ -1,11 +1,19 @@
-const functions = require('firebase-functions');
-const admin = require('./admin');
+const functions = require("firebase-functions");
+const admin = require("./admin");
 
-module.exports = functions.auth.user().onCreate((user) => {
-  var email = user.email;
-  return admin.database().ref('users/' + user.uid).update({
-    email: email && email.toLowerCase ? email.toLowerCase() : email,
-    uid: user.uid,
-    createdAt: new Date().getTime()
-  });
+module.exports = functions.auth.user().onCreate(user => {
+  const { uid, email } = user;
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(uid)
+    .set({
+      email: email && email.toLowerCase ? email.toLowerCase() : email,
+      uid: uid,
+      createdAt: new Date().getTime()
+    })
+    .catch(e => {
+      console.log(e);
+      console.log("There was an error sending welcome email to " + user.email);
+    });
 });
