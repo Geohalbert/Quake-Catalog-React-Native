7ed6ad29a2b77b744034db2ccb35b2bd7a816f21
Hal Peters <geohalbert@gmail.com>
2020-04-02 16:42:02 -0500

    more build updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    es6: true,
+    jest: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "react-native"],
+  rules: {
+    "react/prop-types": "off"
+  }
 };
